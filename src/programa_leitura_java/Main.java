f3d5f4240bbee8ea43950518248f727f36abd519
itamar-gouveia <contato.itamargouveia@gmail.com>
2021-09-14 14:33:04 -0300

    terceira modificação

--- src/programa_leitura_java/Main.java
+++ src/programa_leitura_java/Main.java
@@ -5,8 +5,25 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner in  = new Scanner(System.in);
-		String nome = in.nextLine();
+		String nome, idade, cidade, uf, pais;
+		System.out.println("Por favor digite seu nome:");
+		nome = in.nextLine();
+		System.out.println("Por favor digite sua idade:");
+		idade = in.nextLine();
+		System.out.println("Por favor digite sua cidade:");
+		cidade = in.nextLine();
+		System.out.println("Por favor digite seu estado:");
+		uf = in.nextLine();
+		System.out.println("Por favor digite seu país:");
+		pais = in.nextLine();
 		
+		System.out.println("-----------------------------");
+		System.out.println("Seus dados");
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Cidade: " + cidade);
+		System.out.println("Estado: " + uf);
+		System.out.println("País: " + pais);
 		
 		
 	}
