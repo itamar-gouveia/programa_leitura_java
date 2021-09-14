0e4b335e0ba72ed8c5925184869abbb29cb9bde8
itamar-gouveia <contato.itamargouveia@gmail.com>
2021-09-14 14:06:28 -0300

    commit 1

---
+++ src/programa_leitura_java/Main.java
@@ -0,0 +1,10 @@
+package programa_leitura_java;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in  = new Scanner(System.in);
+		String nome = in.nextLine();
+	}
+}
